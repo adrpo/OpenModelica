d12c404a7a220445bc8913b458e18be849bafd68
Adrian Pop <adrian.pop@liu.se>
2024-03-28 02:12:07 +0100

    fix creation of crml files, add mos file support

--- OMEdit/OMEditLIB/Util/StringHandler.cpp
+++ OMEdit/OMEditLIB/Util/StringHandler.cpp
@@ -1863,6 +1863,8 @@ int StringHandler::getLeadingSpacesSize(QString str)
  */
 bool StringHandler::isFileWritAble(QString filePath)
 {
+  if (filePath.isEmpty())
+    return true;
   QFile file(filePath);
   if (file.exists()) {
     return file.permissions().testFlag(QFile::WriteUser);
